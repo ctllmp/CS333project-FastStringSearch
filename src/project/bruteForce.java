1662461c7ab194b94879caae5ea9e555f056d3bc
yorgos <bal_9030@hotmail.com>
2022-05-20 10:58:21 +0300

    brute force implementation

--- src/project/bruteForce.java
+++ src/project/bruteForce.java
@@ -3,10 +3,28 @@ package project;
 public class bruteForce implements algorithm {
     String name = "Brute Force Algorithm";
 
-
     @Override
-    public void search(String pat, String text){    //TODO finish this part
-
+    public void search(String pat, String text){
+        int textLen = text.length();
+        int patLen = pat.length();
+        boolean found = false;
+        int i;
+        for(i = 0; i < textLen - patLen + 1; i++) {
+            int j;
+            for (j = 0; j < patLen; j++) {
+                if (text.charAt(i + j) != pat.charAt(j))
+                    break;
+            }
+            if (j == patLen) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println(name+" found the pattern at index: " + i);
+        } else {
+            System.out.println(name+" couldn't find the pattern");
+        }
     }
     @Override
     public String getName(){
